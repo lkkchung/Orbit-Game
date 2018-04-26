220a29116864b5ffe6f34bec6079a7b16537843f
Lucas <lucas@Macbizzle.home>
2018-04-25 23:34:59 -0400

    Switching over to use matter attractors plugin

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ const Engine = Matter.Engine;
 const Bodies = Matter.Bodies;
 const World = Matter.World;
 const Vector = Matter.Vector;
+const Body = Matter.Body;
+const Events = Matter.Events;
 
-// Matter.use(
-//   'matter-attractors' // PLUGIN_NAME
-// );
+Matter.use(
+  'matter-attractors' // PLUGIN_NAME
+);
 
 let engine;
 let world;
@@ -32,13 +34,25 @@ function setup() {
 function draw() {
   background(0, 100);
   Engine.update(engine, 60);
+  Events.on(engine, 'collisionStart', collision);
+  sparks.push(new particle(mouseX, mouseY));
+
+  function collision(event) {
+    let pairs = event.pairs;
+    for (let i = 0; i < pairs.length; i++) {
+      let bodyA = pairs[i].bodyA;
+      let bodyB = pairs[i].bodyB;
+    }
+
+  }
 
   fill(100);
   rectMode(CENTER);
   rect(width / 2, height - 10, width, 20);
 
-  for (let q of sparks) {
-    q.render();
+  for (let i = 0; i < sparks.length; i++) {
+    sparks[i].render();
+    i -= sparks[i].update(i);
   }
 }
 
@@ -56,18 +70,31 @@ class particle {
     let yForce = random(coinToss) * random(0, sqrt(0.0025 - xForce * xForce));
     let params = {
       friction: 0.01,
-      mass: 0, //this.rad * 2,
-      restitution: 0.6,
+      mass: this.rad * 2,
+      restitution: 0.9,
       force: {
-        x: 0, //xForce,
-        y: 0, //yForce
+        x: xForce,
+        y: yForce
       }
     };
 
     this.body = Bodies.circle(_x, _y, this.rad, params);
     World.add(engine.world, this.body);
-    console.log(this.body.position.y);
-    console.log(this.body);
+    // console.log(this.body.position.y);
+    // console.log(this.body);
+  }
+
+  update(_i) {
+    let originalRad = this.rad;
+    this.rad -= 0.2;
+    Matter.Body.scale(this.body, this.rad / originalRad, this.rad / originalRad);
+
+    if (this.rad < 1) {
+      removeItem(_i);
+      World.remove(world, this.body);
+      return 1;
+    }
+    return 0;
   }
 
   render() {
@@ -80,4 +107,8 @@ class particle {
       World.remove(engine.world, this.body);
     }
   }
+}
+
+function removeItem(_i) {
+  sparks.splice(_i, 1);
 }
