d0df61ff46b37f95c47ccac9e3d2ade494cb4343
Lucas <lucas@Macbizzle.home>
2018-05-10 21:59:34 -0400

    got controller to send values of power and angle

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,16 +1,49 @@
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+
+//  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+//    Serial.println(F("Failed to boot VL53L0X"));
+    while (1);
+  }
+  // power
+//  Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
 }
 
+
 void loop() {
-  int pot = analogRead(A0); 
-  int IRValue = analogRead(A1); 
+  VL53L0X_RangingMeasurementData_t measure;
+  int dist = 0;
 
- if (pot<=200)
- {
-  int potValue = map(pot,0,200,101,201); 
-  Serial.println(potValue); 
- }
+//    Serial.print("Reading a measurement... ");
+    lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
-  delay(500);   
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    //    Serial.print("Distance (mm): ");
+    dist = measure.RangeMilliMeter;
+  } else {
+    dist = 0;
+  }
+
+  int distValue = map(dist, 45, 120, 0, 12);
+
+  Serial.print(distValue);
+
+  Serial.print(",");
+
+  int pot = analogRead(A0);
+
+  int potValue = map(pot, 691, 374, -45, 45);
+  Serial.println(potValue);
+
+  delay(100);
 }
